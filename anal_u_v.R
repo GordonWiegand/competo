5cb1288f00cb7f733b3ad342a62f7d882b0ec9da
Gordon Wiegand <Gordon.Wiegand@bluewin.ch>
2013-12-02 12:56:10 +0100

    [u und v neu]

--- anal_u_v.R
+++ anal_u_v.R
@@ -1,6 +1,7 @@
-  rm(list=ls())
+rm(list=ls())
+setwd("/Volumes/hd2/131021_briefmarken4/")  
 library(foreign)
-d0 <- as.data.frame(read.csv("/Volumes/hd2/130100_competo2/competo2013.csv"))
+d0 <- as.data.frame(read.spss("bmuv.sav"))
 d0[d0<0] <- NA
 #Impute NAs by estimeated values
 d0$f2a[d0$f2b==1] <- mean(d0$f2a[(d0$f2a>0 & d0$f2a<51)],na.rm=T)
@@ -10,9 +11,13 @@ d0$f2a[d0$f2b==4] <- mean(d0$f2a[(d0$f2a>300 & d0$f2a<501)],na.rm=T)
 d0$f2a[d0$f2b==5] <- mean(d0$f2a[(d0$f2a>500 & d0$f2a<1001)],na.rm=T)
 d0$f2a[d0$f2b==6] <- mean(d0$f2a[(d0$f2a>1000 & d0$f2a<2001)],na.rm=T)
 d0$f2a[d0$f2b==7] <- mean(d0$f2a[d0$f2a>2000],na.rm=T)
-#working data set
-du <- d0[d0$q0==1,c("f2a","gew")] #companies
-dv <- d0[d0$q0==2,c("f2a","gew")] #associations
+#working data sef
+du <- d0[as.character(d0$q0)=="Unternehmen",c("f2a","gew")] #companies
+du[1] <- as.numeric(as.matrix(du[1]))
+du[2] <- as.numeric(as.matrix(du[2]))
+dv <- d0[as.character(d0$q0)=="Vereine",c("f2a","gew")] #associations
+dv[1] <- as.numeric(as.matrix(dv[1]))
+dv[2] <- as.numeric(as.matrix(dv[2]))
 names(du)<-c("bm","w")
 names(dv)<-c("bm","w")
 
@@ -26,39 +31,54 @@ win<-function(dw,tr){
 	weighted.mean(dw$bm,dw$w)
 }
 
-block <- 80
-schritte <- 70
-runden <- 1000
-
-
-d <- du
-library(multicore)
-
-runner <- function(j){
-        out <- numeric(2)
-        outci <- double(runden)
-        nrd <- nrow(d)
-        ss <- j*20
-        #Heyho, simulate!
-        for (i in 1:runden){
-	        ds<-d[sample(nrd,size=ss,replace=T),]
-	        names(ds) <- c("bm","w")
-	        outci[i]<-win(ds,.1)
-                }
-        out[1] <- mean(outci)
-        out[2] <- ((out[1]-(mean(outci)-1.96*sqrt(var(outci))))*100)/out[1]
-        print(schritte)
-        return(out[2])
-        
-}
-for (bl in 1:block){
-mclist <- list()
-l <- read.table("/Volumes/hd2/130100_competo2/out_h.csv")
-for (k in ((bl-1)*schritte+1):(bl*schritte) ) mclist <- c(mclist,k)  
-res<-numeric()
-res <- as.numeric(mclapply(mclist,runner))
-
-write.table(c(l$x,res),"/Volumes/hd2/130100_competo2/out_h.csv")
+#Confidence Intervalls
+winMeanVar <- function(ds,r,tr){ #ds Datensatz, r Runden, tr trimbereich
+    out <- numeric(r)
+    for (i in 1:r){
+        d <- ds[sample(nrow(ds),nrow(ds),replace=T),]
+        out[i] <- win(d,tr)
+    }
+    print((sqrt(var(out))*1.96)/win(du,tr))
 }
 
+print("Unternehmen")
+win(du,.1)
+winMeanVar(du,10000,.15)
 
+print("Vereine")
+win(dv,.1)
+winMeanVar(dv,10000,.15)
+
+#Plots
+#############################################################
+
+library(ggplot2)
+
+
+#Unternehmen
+dPlot <- du[!is.na(du$bm),] #NAs raus
+dPlot$bm[dPlot$bm>300] <- 300 #bei 300 abschneiden wg Lesbarkeit
+jpeg("u_dens.jpg",quality=100)
+    ggplot(dPlot, aes(x=bm)) + 
+        geom_histogram(aes(y=..density..),
+                       binwidth=20,                 # Bins bei 20 Franken
+                       colour="black", fill="white") +
+        xlab("Briefmarken in CHF") +
+        ylab("Dichte") +
+        theme(axis.text.y=element_blank()) +
+        geom_density(alpha=.2, fill="yellow") #alpha ist "Durchsichtigkeit"
+dev.off()
+
+#Vereine
+dPlot <- dv[!is.na(dv$bm),] #NAs raus
+dPlot$bm[dPlot$bm>300] <- 300 #bei 300 abschneiden wg Lesbarkeit
+jpeg("v_dens.jpg",quality=100)
+    ggplot(dPlot, aes(x=bm)) + 
+        geom_histogram(aes(y=..density..),
+                       binwidth=20,                 # Bins bei 20 Franken
+                       colour="black", fill="white") +
+        xlab("Briefmarken in CHF") +
+        ylab("Dichte") +
+        theme(axis.text.y=element_blank()) +
+        geom_density(alpha=.2, fill="yellow") #alpha ist "Durchsichtigkeit"
+dev.off()
